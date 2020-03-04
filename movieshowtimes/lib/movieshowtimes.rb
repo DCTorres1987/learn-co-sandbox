6e4e23ef6212bf385c6520aaa8caa5ddfcaa2d0f
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-03-04 01:57:32 +0000

    first commit

---
+++ movieshowtimes/lib/movieshowtimes.rb
@@ -0,0 +1,6 @@
+require "movieshowtimes/version"
+
+module Movieshowtimes
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ movieshowtimes/lib/movieshowtimes/version.rb
@@ -0,0 +1,3 @@
+module Movieshowtimes
+  VERSION = "0.1.0"
+end
