e5fd730b1536baf8ceb8331c0d6a33f13a9be36e
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-03-08 22:21:34 +0000

    save to github

--- movieshowtimes/lib/movieshowtimes/showtimes.rb
+++ movieshowtimes/lib/movieshowtimes/showtimes.rb
@@ -1,23 +1,32 @@
 class Movieshowtimes::Showtimes
-  attr_accessor :tmsId, :rootId, :subType, :title, :releaseYear, :releaseDate, :titleLang, :descriptionLang, :entityType, :genres, :audience, :longDescription, :shortDescription, :topCast, :directors, :officialUrl, :ratings, :advisories, :runTime, :animation, :preferredImage, :showtimes
+  attr_accessor :tmsId, :rootId, :subType, :title, :releaseYear, :releaseDate, :titleLang, :descriptionLang, :entityType, :genres, :audience, :longDescription, :shortDescription, :topCast, :directors, :officialUrl, :qualityRating, :ratings, :advisories, :runTime, :animation, :preferredImage, :showtimes
   
   @@all = []
   
   def initialize(hash)
     hash.each {|k, v| self.send("#{k}=", v)}
     @@all << self
+    binding.pry
   end
-
-  def display_movie_listing
-    b = self.all[index]
-binding.pry
-  end
-
+  
   def self.all
     @@all
   end
 
+# def display_movie_listing
+#   self.all.select {|theater| binding.pry}
+  
+# end  
+  # def display_movie_listing
+  #   # @@all.showtimes.each do |movie|
+  #   #   # puts "Movie: #{movie.}"
+  #   #   #puts interpolate movies showing
+  #   # end
+  #   binding.pry
+  # end
+
   def self.destroy_all
     @@all.clear
   end
+  
 end
