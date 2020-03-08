56d6419b75768f4f153d633d8ecb4ade03ddc7dc
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-03-08 20:26:57 +0000

    save to github

--- movieshowtimes/lib/movieshowtimes/api.rb
+++ movieshowtimes/lib/movieshowtimes/api.rb
@@ -1,9 +1,8 @@
 class Movieshowtimes::API 
-  def fetch(zip)
+  def fetch(zip, date)
     key = "tbvdtyjja5sq5ajccsbrek6a"
-    url = "http://data.tmsapi.com/v1.1/movies/showings?startDate=2020-03-06&zip=#{zip}&api_key=#{key}"
-    response = HTTParty.get(url)
-    binding.pry
+    url = "http://data.tmsapi.com/v1.1/movies/showings?startDate=#{date}&zip=#{zip}&api_key=#{key}"
+    theater = HTTParty.get(url)
+    end
   end 
-
 end
--- movieshowtimes/lib/movieshowtimes/cli.rb
+++ movieshowtimes/lib/movieshowtimes/cli.rb
@@ -2,6 +2,10 @@ class Movieshowtimes::CLI
   def call 
     puts "Welcome to Movie Show Times! Please input a zipcode for showtimes in your area."
     zip = gets.chomp
-    Movieshowtimes::API.new.fetch(zip)
-  end 
+    puts "Please enter showtime date (yyyy-mm-dd)"
+    date = gets.chomp
+    Movieshowtimes::API.new.fetch(zip, date)
+  end
+  
+  
 end
