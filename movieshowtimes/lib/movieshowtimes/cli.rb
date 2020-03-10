75c19128dd2da3af3aefec590f952d59161284a9
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-03-10 20:30:01 +0000

    save to github

--- movieshowtimes/lib/movieshowtimes/cli.rb
+++ movieshowtimes/lib/movieshowtimes/cli.rb
@@ -24,12 +24,20 @@ class Movieshowtimes::CLI
     puts  "        Y8P"                                      
     puts  "                                         "       
     puts  "                                         "            
-    puts  " .d8888b88888888b.  .d88b. 88888b.d88b.  8888b."
-    puts  " d88P   888888 88bd8P   Y8b888 888  '88b     88b"
-    puts  " 888    888888 888888888888888 888  888.d888888" 
-    puts  " Y88b.  888888 888Y8b.     888 888  8888888 888" 
-    puts  "  Y8888P888888 888  'Y8888 888 888  888'Y888888" 
+    puts  " .d8888b88888888b.  .d88b. 88888b.d88b.  8888b.".colorize(:red)
+    puts  " d88P   888888 88bd8P   Y8b888 888  '88b     88b".colorize(:red)
+    puts  " 888    888888 888888888888888 888  888.d888888".colorize(:red) 
+    puts  " Y88b.  888888 888Y8b.     888 888  8888888 888".colorize(:red) 
+    puts  "  Y8888P888888 888  'Y8888 888 888  888'Y888888".colorize(:red) 
     puts  "***************************************************"
+    puts "****************************************"
+    puts "*******************************"
+    puts "***********************"
+    puts "******************"
+    puts "*************"
+    puts "*********"
+    puts "******"
+    puts "***"
     Movieshowtimes::Showtimes.display_movie_listing
   end
   
@@ -40,11 +48,9 @@ class Movieshowtimes::CLI
     
       if choice == "yes"
         
-        puts "Please enter a 5-digit zipcode."
-        zip = gets.chomp
-        puts "Please enter showtime date (yyyy-mm-dd)"
-        date = gets.chomp
-        Movieshowtimes::API.new.fetch(zip, date)
+        showtimes_search
+        display_movie_showtimes
+        another_search
       elsif choice == "no"
         goodbye
       else
--- movieshowtimes/lib/movieshowtimes/showtimes.rb
+++ movieshowtimes/lib/movieshowtimes/showtimes.rb
@@ -14,18 +14,22 @@ class Movieshowtimes::Showtimes
 
   def self.display_movie_listing
       self.all.each do |movie|
-      puts "****************************************"
-      puts "*******************************"
-      puts "***********************"
+      puts "                        "
       puts "Movie: #{movie.title}"
+      puts "                        "
       puts "Genres:  #{movie.genres}"
       puts "Description: #{movie.longDescription}"
-      puts "****************************************"
+      puts "****************************************".colorize(:red)
       puts "Showtimes: "
       movie.showtimes.each do |s|
-        puts s["theatre"]["name"]
+        puts "                      "
+        puts s["theatre"]["name"].colorize(:yellow)
+        puts "______________________"
         puts s["dateTime"].split("T")
+        puts "                      "
+        puts "Please click here to purchase your ticket."
         puts s["ticketURI"]
+        puts "-------------------------------------------"
       end 
       #Start by writing out what you want it to look like in plain English
       # Movie: Movie titlev <--- attribute that you need to access
