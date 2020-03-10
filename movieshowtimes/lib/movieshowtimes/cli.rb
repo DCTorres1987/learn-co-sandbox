110ecb926e74efb8542f836e7469ec388c8c44f6
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-03-10 18:54:11 +0000

    save to github

--- movieshowtimes/lib/movieshowtimes/cli.rb
+++ movieshowtimes/lib/movieshowtimes/cli.rb
@@ -1,7 +1,8 @@
 class Movieshowtimes::CLI 
   def call 
-    welcome
-    showtimes_search
+      welcome
+      showtimes_search
+      display_movie_showtimes
   end
   
   def welcome 
@@ -14,19 +15,20 @@ class Movieshowtimes::CLI
     puts "Please enter showtime date (yyyy-mm-dd)"
     date = gets.chomp
     Movieshowtimes::API.new.fetch(zip, date)
-    Movieshowtimes::Showtimes.display_movie_listing
   end
   
   def display_movie_showtimes
     
-    puts  "   d8b"                                      
-    puts  "   Y8P"                                      
-    puts  "                                         "                                             
+    puts  "        d8b"                                      
+    puts  "        Y8P"                                      
+    puts  "                                         "       
+    puts  "                                         "            
     puts  " .d8888b88888888b.  .d88b. 88888b.d88b.  8888b."
-    puts  " d88P   888888 88bd8P  Y8b888 888  '88b     88b"
-    puts  " 888    888888 88888888888888 888   888.d888888" 
-    puts  "Y88b.   888888 888Y8b.    888 888   8888888 888" 
-          "Y8888P888888  888 "Y8888  888 888  888"Y888888 
-  
-  
+    puts  " d88P   888888 88bd8P   Y8b888 888  '88b     88b"
+    puts  " 888    888888 888888888888888 888  888.d888888" 
+    puts  " Y88b.  888888 888Y8b.     888 888  8888888 888" 
+    puts  "  Y8888P888888 888  'Y8888 888 888  888'Y888888" 
+    puts  "***************************************************"
+    Movieshowtimes::Showtimes.display_movie_listing
+  end
 end
