a2b2bba2ae9fc4d218cb5e9081e7e2a8ab75660a
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-03-10 21:26:13 +0000

    save to github

--- movieshowtimes/lib/movieshowtimes/cli.rb
+++ movieshowtimes/lib/movieshowtimes/cli.rb
@@ -13,31 +13,31 @@ class Movieshowtimes::CLI
   def showtimes_search
     puts "Please enter a 5-digit zipcode."
     zip = gets.chomp
-    puts "Please enter showtime date (yyyy-mm-dd)"
-    date = gets.chomp
-    Movieshowtimes::API.new.fetch(zip, date)
+    if zip.length == 5 
+      puts "Please enter showtime date (yyyy-mm-dd)"
+      date = gets.chomp
+      Movieshowtimes::API.new.fetch(zip, date)
+    else
+      puts "Invalid Zip!  Please type again"
+      showtimes_search
+    end
   end
   
   def display_movie_showtimes
-    
-    puts  "        d8b"                                      
-    puts  "        Y8P"                                      
-    puts  "                                         "       
+    puts "                                                               "
+    puts  "        d8b".colorize(:red)                                   
+    puts  "        Y8P".colorize(:red)                                 
     puts  "                                         "            
     puts  " .d8888b88888888b.  .d88b. 88888b.d88b.  8888b.".colorize(:red)
     puts  " d88P   888888 88bd8P   Y8b888 888  '88b     88b".colorize(:red)
     puts  " 888    888888 888888888888888 888  888.d888888".colorize(:red) 
     puts  " Y88b.  888888 888Y8b.     888 888  8888888 888".colorize(:red) 
     puts  "  Y8888P888888 888  'Y8888 888 888  888'Y888888".colorize(:red) 
-    puts  "***************************************************"
-    puts "****************************************"
-    puts "*******************************"
-    puts "***********************"
-    puts "******************"
-    puts "*************"
-    puts "*********"
-    puts "******"
-    puts "***"
+    puts "*********************************************************"
+    puts "********************************************"
+    puts "*****************************"
+    puts "***********"
+   
     Movieshowtimes::Showtimes.display_movie_listing
   end
   
