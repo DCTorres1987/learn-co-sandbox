3b81f40b5f9968010d254cffea10aa2b38adf1e6
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-03-12 01:24:47 +0000

    save to github

--- movieshowtimes/lib/movieshowtimes/api.rb
+++ movieshowtimes/lib/movieshowtimes/api.rb
@@ -1,8 +1,20 @@
 class Movieshowtimes::API 
-  def fetch(zip, date)
+  def self.fetch(zip, date)
     key = "tbvdtyjja5sq5ajccsbrek6a"
     url = "http://data.tmsapi.com/v1.1/movies/showings?startDate=#{date}&zip=#{zip}&api_key=#{key}"
-    theater = HTTParty.get(url)
-    theater.each {|theater_hash| Movieshowtimes::Showtimes.new(theater_hash)}
-    end
+    response = HTTParty.get(url)
+    response.each do |h|
+        # I need to iterate through the outer hash and grab the following fields 
+        # title (belongs to showtimes class)
+        # genres (belongs to showtimes class)
+        # longDescription (belongs to showtimes class)
+      h.each do |theatre|
+        # I need to iterate through the inner hash and grab the following fields
+        # Theatre => Name (belongs to theatre class)
+        # dateTime  (belongs to showtimes class)
+        # ticketURI (belongs to showtimes class)
+        binding.pry
+      end   
+    end  
+  end
 end
